5f5c0ab97d04cf590588bc423d8e5e3c7bc70a89
mykewayner <mykewayner@gmail.com>
2025-03-17 21:24:09 -0300

    commit

---
+++ src/test/java/HotelTest.java
@@ -0,0 +1,21 @@
+import org.example.Hotel;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HotelTest {
+
+    @Test
+    public void retornaNomeHotel() {
+        Hotel.getInstance().setNomeHotel("Hotel Academia");
+        assertEquals("Hotel Academia", Hotel.getInstance().getNomeHotel());
+    }
+
+    @Test
+    public void retornaNomeCliente() {
+        Hotel.getInstance().setNomeCliente("Myke Wayner");
+        assertEquals("Myke Wayner", Hotel.getInstance().getNomeCliente());
+    }
+
+}
